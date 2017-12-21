df89e870cd6264f2ba1f8921d921ae984aca0f2c
elmihailol <elmihailol@gmail.com>
2017-12-21 23:19:18 +0700

    Add files via upload

--- WildCardLib.py
+++ WildCardLib.py
@@ -3,6 +3,8 @@ from keras import Sequential
 from keras.layers import LSTM, Dense
 import matplotlib.pyplot as plt
 
+
+# Create dataset, where X = [n-look_back, n-look_back+1, ...., n-1] and Y = [n]
 def create_dataset(dataset, look_back=1):
     dataX, dataY = [], []
     for i in range(len(dataset) - look_back - 1):
@@ -12,30 +14,37 @@ def create_dataset(dataset, look_back=1):
     return numpy.array(dataX), numpy.array(dataY)
 
 
+# Make reccurent predictions based on some model
 def extended_this(model, trainX, trainY, look_back, multi=2):
+    # Create dataset in comfortable type
     X = []
     Y = []
     for i in range(len(trainX)):
         X.append(trainX[i])
     for i in range(len(trainY)):
         Y.append(trainY[i])
-    last_X =[trainX[len(trainX)-1]]
+    # Make len(X) * multi predictions
     for i in range(int(len(X) * multi)):
-        print(X[0])
-        buf = numpy.array(X[len(X)-1])
-        print(buf)
-        newX = numpy.array([buf])
-        last_y = model.predict(newX)
-        # newX = numpy.array([trainX[len(trainX)-1]])
-        # last_y = model.predict(newX)
+        # Show every 100 iters
+        if i % 100 == 0:
+            print(i)
+        # Get last element in X. last
+        last_x = numpy.array([X[len(X) - 1]])
+        # Make prediction based on last_x
+        last_y = model.predict(last_x)
+        # Create list which will contains new X = [n-look_back, n-look_back+1, ...., n-1]
         merge = []
+        # Fill that new X
         for a in range(look_back - 1, 0, -1):
             merge.append(Y[i - a])
+        # Add new prediction to new X
         merge.extend([last_y[0]])
+        # Add new data to datasets
         Y.append(last_y[0])
-        print(numpy.array(merge))
         X.append(numpy.array(merge))
+    # Return new datalist
     return Y
+
 
 def example_LSTM():
     data = []
@@ -44,7 +53,7 @@ def example_LSTM():
 
     print(data)
     look_back = 20
-    trainX, trainY = WildCardLib.create_dataset(data, look_back)
+    trainX, trainY = create_dataset(data, look_back)
 
     print(trainX)
     model = Sequential()
@@ -56,10 +65,8 @@ def example_LSTM():
     model.fit(trainX, trainY, epochs=2, batch_size=1, verbose=2)
     trainPredict = model.predict(trainX)
     new_len = int(len(trainX) * 1)
-    Y = WildCardLib.extended_this(model=model, trainX=trainX[0:new_len], trainY=trainY[0:new_len], look_back=look_back,
-                                  multi=1.5)
-    # plt.plot(trainPredict)
-
+    Y = extended_this(model=model, trainX=trainX[0:new_len], trainY=trainY[0:new_len], look_back=look_back,
+                      multi=3)
     plt.plot(Y)
     plt.plot(trainY)
     plt.show()
